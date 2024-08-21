302a3148cad96bf8075096a22623646921d6fda8
GHuyHuynh <huy.huynh@dal.ca>
2024-08-20 22:05:51 -0300

    [Add] AWS lambda function

--- main.py
+++ main.py
@@ -13,22 +13,28 @@ HOURS_2: List[int] = [11, 13]
 HOURS_3: List[int] = [13, 15]
 HOURS_4: List[int] = [15, 17]
 
-day: str = limit_date_object['day']
-month: str = limit_date_object['month']
-year: str = limit_date_object['year']
 
-url: str = generate_url(day, month, year, HOURS_2[0], HOURS_2[1])
+def lambda_handler(event, context):
+   day: str = limit_date_object['day']
+   month: str = limit_date_object['month']
+   year: str = limit_date_object['year']
 
-email: str = os.getenv('EMAIL')
-password: str = os.getenv('PASSWORD')
+   url: str = generate_url(day, month, year, HOURS_2[0], HOURS_2[1])
 
+   email: str = os.getenv('EMAIL')
+   password: str = os.getenv('PASSWORD')
 
-result: int = book_room(email, password, url)
+   result: int = book_room(email, password, url)
 
-if result == 0:
-   print("Room booked successfully!")
-
-else:
-   print("An error occurred while booking the room.")
-   print('The error code is:', result)
+   if result == 0:
+      return {
+         'statusCode': 200,
+         'body': 'Room booked successfully!'
+      }
+   
+   else:
+      return {
+         'statusCode': 500,
+         'body': f'An error occurred while booking the room. Error code: {result}'
+      }
 
